2ca9c5d691a868ec211d86d7b4395bbef65a535b
flucas97 <feijolucas1997@gmail.com>
2020-05-24 18:20:26 -0300

    use for in slice

--- exercises/osArgs/main.go
+++ exercises/osArgs/main.go
@@ -8,11 +8,16 @@ import (
 func main() {
 	var args []string
 	args = os.Args
+	/*
+		fmt.Println("Path", args[0])
+		fmt.Println("1th", args[1])
+		fmt.Println("2th", args[2])
+		fmt.Println("3th", args[3])
+	*/
 
-	fmt.Println("Path", args[0])
-	fmt.Println("1th", args[1])
-	fmt.Println("2th", args[2])
-	fmt.Println("3th", args[3])
+	for i := 0; i < len(args); i++ {
+		fmt.Println("args: ", args[i])
+	}
 
 	fmt.Printf("%#v\n", args)
 }
