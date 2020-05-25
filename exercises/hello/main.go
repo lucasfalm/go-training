612a3a2ddc5c99b5e5e30f962f1b50314e31e5a4
flucas97 <feijolucas1997@gmail.com>
2020-05-25 18:22:29 -0300

    use argument index in printf

--- exercises/hello/main.go
+++ exercises/hello/main.go
@@ -6,18 +6,22 @@ import (
 
 func main() {
 	var (
-		age     int
-		lastAge int
-		name    string
-		phrase  = "Ola mundo!"
+		age int
+		// lastAge int
+		name   string
+		phrase = "Ola mundo!"
 	)
 	fmt.Printf("%q\n", phrase)
 	fmt.Printf("%T\n", phrase)
 
-	age, lastAge, name = 22, 21, "Lucas"
+	age, name = 22, "Lucas"
+
 	fmt.Printf("Seu nome é %s, e sua idade é %d\n", name, age)
 
-	age, lastAge = lastAge, age
+	fmt.Printf("Sua idade é %[2]v, e seu nome é %[1]v\n", name, age)
+	/*
+		age, lastAge = lastAge, age
 
-	fmt.Println(age)
+		fmt.Println(age)
+	*/
 }
