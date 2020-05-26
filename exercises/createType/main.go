dc85e8d8d3b80ab2fb5055b716b2408ad2bc24d4
flucas97 <feijolucas1997@gmail.com>
2020-05-26 18:37:35 -0300

    use type conversion

--- exercises/createType/main.go
+++ exercises/createType/main.go
@@ -7,7 +7,9 @@ import (
 func main() {
 	type phoneType int64
 
-	var phone phoneType = 32323232
+	var phone phoneType
+
+	phone = phoneType(32323232)
 
 	fmt.Printf("My phone is %v and the type is: %T\n", phone, phone)
 }
