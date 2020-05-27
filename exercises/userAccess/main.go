bf2a4b22991eea52a879ec1b896177bcdaba4a4b
flucas97 <feijolucas1997@gmail.com>
2020-05-27 11:07:50 -0300

    create controll access with if statement

---
+++ exercises/userAccess/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var (
+		args          = os.Args
+		validUser     = "flucas97"
+		validPassword = "123456"
+		userName      string
+		password      string
+	)
+
+	const (
+		usage    = "Usage: [username] [password]"
+		denied   = "Access denied for %v\n"
+		errPwd   = "Invalid password for %v\n"
+		accessOK = "Access granted to %v\n"
+	)
+
+	if len(args) != 3 {
+		fmt.Println(usage)
+		return
+	}
+
+	userName, password = os.Args[1], os.Args[2]
+
+	if userName != validUser {
+		fmt.Printf(denied, userName)
+	} else if password != validPassword {
+		fmt.Printf(errPwd, userName)
+	} else {
+		fmt.Printf(accessOK, userName)
+	}
+}
