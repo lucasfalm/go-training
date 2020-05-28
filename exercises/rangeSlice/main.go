7d79ee6ce50e9d77ce88bc8d2501582e94f4ab5a
flucas97 <feijolucas1997@gmail.com>
2020-05-28 10:53:46 -0300

    use var declaration for range slice

--- exercises/rangeSlice/main.go
+++ exercises/rangeSlice/main.go
@@ -6,7 +6,14 @@ import (
 )
 
 func main() {
-	for i, v := range os.Args[1:] {
-		fmt.Printf("The index %v has the value %v\n", i + 1, v)
+	var (
+		i int
+		v string
+	)
+	for i, v = range os.Args[1:] {
+		fmt.Printf("The index %v has the value %v\n", i+1, v)
+	}
+	if v != "" {
+		fmt.Println("I Still have the value of last index: ", v)
 	}
 }
