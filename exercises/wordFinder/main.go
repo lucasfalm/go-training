4f6b10b25a9ce422061af6ab3c4a92909cb1c579
flucas97 <feijolucas1997@gmail.com>
2020-05-28 21:50:03 -0300

    word finder with nested for range loop

---
+++ exercises/wordFinder/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	const corpus = "lazy cat jumps again and again and again"
+	var (
+		words = strings.Fields(corpus)
+		query = os.Args[1:]
+		flag  int
+	)
+	if len(query) > 1 {
+		for _, v := range query {
+			for i, w := range words {
+				if v == w {
+					fmt.Printf("The %q is at position %v: %q\n", v, i, w)
+					flag++
+				} else {
+					if i == (len(words)-1) && flag == 0 {
+						fmt.Printf("We dont find any word in match. Try again\n")
+						break
+					} else {
+						continue
+					}
+				}
+			}
+		}
+	} else {
+		fmt.Println("Please, write at least one word to search.")
+	}
+}
