3c677c3444175540c6dcb667d9efe4b8ef467650
flucas97 <feijolucas1997@gmail.com>
2020-05-29 22:25:11 -0300

    random feelings coming from array

---
+++ exercises/moodFeelings/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main() {
+	var (
+		args = os.Args[1:]
+		name string
+	)
+
+	const (
+		happy  = "feels happy"
+		hungry = "feels hungry"
+		mad    = "feels mad"
+		sad    = "feels sad"
+	)
+
+	mood := [...]string{happy, hungry, mad, sad}
+
+	if len(args) == 1 {
+		name = args[0]
+
+		rand.Seed(time.Now().UnixNano())
+		sNum := rand.Intn(len(mood))
+
+		fmt.Printf("%s %v\n", name, mood[sNum])
+
+	} else {
+		fmt.Println("[Please, write a name]")
+	}
+}
