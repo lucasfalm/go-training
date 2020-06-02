e41048ff36dd6473789ba0503686e3d1c0a89ec3
flucas97 <feijolucas1997@gmail.com>
2020-06-01 21:37:09 -0300

    print clock

--- exercises/clockChallange/main.go
+++ exercises/clockChallange/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	type (
@@ -87,12 +90,33 @@ func main() {
 		"███",
 	}
 
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
 	digits := [...]placeholder{zero, one, two, three, four, five, six, seven, eight, nine}
 
-	for line := range digits[0] {
-		// Print a line for each placeholder in digits
-		for digit := range digits {
-			fmt.Print(digits[digit][line], "  ")
+	now := time.Now()
+	hour, min, sec := now.Hour(), now.Minute(), now.Second()
+
+	fmt.Printf("hour: %d, min: %d, sec: %d\n", hour, min, sec)
+
+	// [8][5]string
+	clock := [...]placeholder{
+		// extract the digits: 17 becomes, 1 and 7 respectively
+		digits[hour/10], digits[hour%10],
+		colon,
+		digits[min/10], digits[min%10],
+		colon,
+		digits[sec/10], digits[sec%10],
+	}
+
+	for line := range clock[0] {
+		for digit := range clock {
+			fmt.Print(clock[digit][line], "  ")
 		}
 		fmt.Println()
 	}
