a383526d57ed6eab864b2791e1547f501b3269c5
flucas97 <feijolucas1997@gmail.com>
2020-06-03 20:23:26 -0300

    use copy instead range the slice

--- exercises/makeSlice/main.go
+++ exercises/makeSlice/main.go
@@ -12,11 +12,11 @@ func main() {
 		mMap = []string{"a", "b", "c", "d", "e"}
 	)
 	// define lenght and capacity of slice, 0 and 10
-	tasks := make([]string, 0, 10)
+	tasks := make([]string, len(mMap), 10)
 	// here my backing array is always the same, because the capacity of my backing array is 10
-	for _, y := range mMap {
-		tasks = append(tasks, y)
-	}
+	copy(tasks, mMap)
 
 	s.Show("Tasks", tasks)
+
+	s.Show("mMap", mMap)
 }
