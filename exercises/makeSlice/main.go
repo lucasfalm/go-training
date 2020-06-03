3bed935ab23852898d11e71c799ea1ae07e2583e
flucas97 <feijolucas1997@gmail.com>
2020-06-03 20:01:58 -0300

    some comments in makeslice

--- exercises/makeSlice/main.go
+++ exercises/makeSlice/main.go
@@ -11,9 +11,9 @@ func main() {
 	var (
 		mMap = []string{"a", "b", "c", "d", "e"}
 	)
-
+	// define lenght and capacity of slice, 0 and 10
 	tasks := make([]string, 0, 10)
-
+	// here my backing array is always the same, because the capacity of my backing array is 10
 	for _, y := range mMap {
 		tasks = append(tasks, y)
 	}
