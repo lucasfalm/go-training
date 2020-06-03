0e3d4691e41f88f5f2cc3f40caec2eca45bfe088
flucas97 <feijolucas1997@gmail.com>
2020-06-03 17:25:50 -0300

    some refactor

--- exercises/cannabisSlice/main.go
+++ exercises/cannabisSlice/main.go
@@ -19,6 +19,22 @@ func main() {
 	sativa = append(sativa, "Cannalope Haze", "Sour Kosher")
 	indica = append(indica, "Afghan Kush", "Quick Kush")
 
+	if len(args) >= 1 {
+		cType := args[0]
+
+		if cType == "sativa" {
+			for _, v := range args[1:] {
+				sativa = sativa.addCannabis(v)
+			}
+		} else if cType == "indica" {
+			for _, v := range args[1:] {
+				indica = indica.addCannabis(v)
+			}
+		} else {
+			fmt.Println("Choose [indica] [sativa]")
+		}
+	}
+
 	fmt.Println("Sativas:")
 	for _, v := range sativa {
 		fmt.Printf("%v\n", v)
@@ -27,11 +43,6 @@ func main() {
 	for _, z := range indica {
 		fmt.Printf("%v\n", z)
 	}
-	if len(args) >= 1 {
-		sativa = sativa.addCannabis(args[0])
-	}
-
-	fmt.Println(sativa)
 }
 
 func (c cannabis) addCannabis(cName string) cannabis {
