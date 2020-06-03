7beeb8d6e55412d464818ba10a8d512fba001662
flucas97 <feijolucas1997@gmail.com>
2020-06-03 10:47:18 -0300

    add comments to read

--- exercises/paginationSlice/main.go
+++ exercises/paginationSlice/main.go
@@ -7,13 +7,19 @@ import (
 func main() {
 	items := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i"}
 
-	const pageSize = 4
-	l := len(items)
+	const (
+		pageSize = 4
+	)
+	var (
+		l = len(items)
+	)
+
 	for from := 0; from < l; from += pageSize {
 		to := from + pageSize
 		if to > l {
 			to = l
 		}
+		// slicing a slice
 		currentPage := items[from:to]
 
 		s.Show("", currentPage)
