0431ce88a2f55007183be9c414dcf48b3aae07f8
flucas97 <feijolucas1997@gmail.com>
2020-06-03 14:06:32 -0300

    refactor cannabis slice

--- exercises/slices/main.go
+++ exercises/slices/main.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	sativa, indica := []string{"x", "y"}, []string{"z", "w"}
+	type (
+		cannabis []string
+	)
+	var (
+		sativa cannabis
+		indica cannabis
+	)
+	sativa = append(sativa, "Cannalope Haze", "Sour Kosher")
+	indica = append(indica, "Afghan Kush", "Quick Kush")
 
 	fmt.Println("Sativas:")
 	for _, v := range sativa {
 		fmt.Printf("%v\n", v)
 	}
-	fmt.Println("Indicas:")
+	fmt.Println("\n\nIndicas:")
 	for _, z := range indica {
 		fmt.Printf("%v\n", z)
 	}
