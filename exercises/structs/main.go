0f63e400a2dbd4bd6e1aee0677d3a0b26a30efdf
flucas97 <feijolucas1997@gmail.com>
2020-06-05 18:07:30 -0300

    some refactor and use map inside struct

--- exercises/structs/main.go
+++ exercises/structs/main.go
@@ -7,6 +7,7 @@ type (
 	flowers struct {
 		name string
 		thc  int
+		geo  map[string]int // declare a map inside a struct
 	}
 	// Define cannabis struct
 	cannabis struct {
@@ -19,29 +20,40 @@ func main() {
 	// Create new types of cannabis using struct type
 	cSativa, cIndica := cannabis{name: "Sativa"}, cannabis{name: "Indica"}
 
+	// Example of geo
+	eX := map[string]int{
+		"Brasil": 2, // countr and qtde of this flower at this country
+	}
 	// Add new sativa and indicas flowers
-	cSativa = cSativa.updateFlower("Gorilla Haze", 27)
-	cIndica = cIndica.updateFlower("Notherland", 22)
+	cSativa, cIndica = cSativa.updateFlower("Gorilla Haze", 27, eX), cIndica.updateFlower("Notherland", 22, eX)
 
 	// Printing the flowers for each type
 	fmt.Println("\nThe Sativa flowers are:")
 	for _, f := range cSativa.flowers {
 		fmt.Printf("Name: %v ------- THC: %v\n", f.name, f.thc)
+		for i, v := range f.geo {
+			fmt.Printf("Country: %v ----- Qtde: %v\n\n", i, v)
+		}
 	}
-
+	fmt.Println("----------------------------")
 	fmt.Println("\n\nThe Indica flowers are:")
+
 	for _, g := range cIndica.flowers {
 		fmt.Printf("Name:%v ------- THC: %v\n", g.name, g.thc)
+		for i, v := range g.geo {
+			fmt.Printf("Country: %v ----- Qtde: %v\n\n", i, v)
+		}
 	}
-
 }
 
 // Function to add new flowers for cannabis type
-func (c cannabis) updateFlower(n string, thc int) cannabis {
+func (c cannabis) updateFlower(n string, thc int, g map[string]int) cannabis {
 	nF := flowers{
 		name: n,
 		thc:  thc,
+		geo:  make(map[string]int), // Initializing map
 	}
-	c.flowers = append(c.flowers, nF)
+	nF.geo = g                        // Give a value to the map
+	c.flowers = append(c.flowers, nF) // Insert every info into flower
 	return c
 }
