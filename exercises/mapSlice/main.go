2b098599ce6fc2956e76be57fccae683b57b9029
flucas97 <feijolucas1997@gmail.com>
2020-06-04 22:48:13 -0300

    dicionary with maps

---
+++ exercises/mapSlice/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var (
+		ptEn = map[string]string{}
+		args = os.Args[1:]
+		flag bool
+	)
+
+	ptEn["Olá"], ptEn["Carro"] = "Hello", "Car"
+
+	if len(args) == 1 {
+		q := args[0]
+	scope:
+		for i, v := range ptEn {
+			if q == i {
+				fmt.Printf("The word %v is %v in english\n", q, v)
+				flag = true
+				break scope
+			} else {
+				continue
+			}
+		}
+		switch flag {
+		case false:
+			fmt.Println("No word find")
+		}
+	} else {
+		fmt.Println("Write one word")
+	}
+}
