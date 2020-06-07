5cf5a83bdd08e0293a09c04b90b761c986f8893e
flucas97 <feijolucas1997@gmail.com>
2020-06-07 13:41:47 -0300

    refactor create examples of geo

--- exercises/structs/main.go
+++ exercises/structs/main.go
@@ -19,15 +19,10 @@ type (
 func main() {
 	// Create new types of cannabis using struct type
 	cSativa, cIndica := cannabis{name: "Sativa"}, cannabis{name: "Indica"}
+	// Create examples of geo
+	eX := createGeoEx("France", 200)
+	eY := createGeoEx("Brazil", 2500)
 
-	// Example of geo
-	eX := map[string]int{
-		"Brazil": 2500, // country and qtde of this flower at this country
-	}
-
-	eY := map[string]int{
-		"France": 200, // country and qtde of this flower at this country
-	}
 	// Add new sativa and indicas flowers
 	cSativa.updateFlower("Gorilla Haze", 27, eX)
 	cIndica.updateFlower("Notherland", 22, eY)
@@ -54,11 +49,19 @@ func (c *cannabis) updateFlower(n string, thc int, g map[string]int) {
 	c.flowers = append(c.flowers, nF) // Insert every info into flower
 }
 
+// Function to print all flowers of Cannabis
 func (c *cannabis) printFlowers() {
 	for _, f := range c.flowers {
 		fmt.Printf("Name: %v ------- THC: %v\n", f.name, f.thc)
 		for c, q := range f.geo {
 			fmt.Printf("Country: %v ----- Qtde: %v\n\n", c, q)
 		}
+	}
+}
+
+// Function to create examples of geo (maps)
+func createGeoEx(c string, q int) map[string]int {
+	return map[string]int{
+		c: q, // country and qtde of this flower at this country
 	}
 }
