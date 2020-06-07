b7d702cf9801a9e6cb9f283bfc552eb8465e9931
flucas97 <feijolucas1997@gmail.com>
2020-06-07 13:29:03 -0300

    add exercises

--- exercises/pointers/main.go
+++ exercises/pointers/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type mT struct {
+	num int
+}
+
 func main() {
 	var (
 		counter byte = 10
@@ -12,4 +16,16 @@ func main() {
 
 	fmt.Printf("The variable p stores the memory address %v, and its value is %v.\n", p, v)
 	fmt.Printf("Its type is %T, and its value is of type %T.\n", p, v)
+
+	g := mT{num: 1}
+	fmt.Println(g.num)
+	g.addOne()
+	fmt.Println(g.num)
+	fmt.Printf("Original g memory position: %p\n", &g)
+}
+
+func (c *mT) addOne() {
+	c.num++
+	fmt.Printf("Memory position %p\n", &c)
+	fmt.Printf("Points to %p\n", c)
 }
