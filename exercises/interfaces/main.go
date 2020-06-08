4198a121132f51fd58adaaf7c337de1d47cbcb4a
flucas97 <feijolucas1997@gmail.com>
2020-06-08 20:24:15 -0300

    use interface as bool checker for a method inside a type

--- exercises/interfaces/main.go
+++ exercises/interfaces/main.go
@@ -12,18 +12,19 @@ type (
 		color string
 	}
 
-	moto struct {
-		name  string
-		color string
+	airplane struct {
+		number string
+		horses string
 	}
 )
 
 func main() {
 	var (
-		c, m = car{name: "Golf", color: "Black"}, moto{name: "Bis", color: "Blue"}
+		c, m = car{name: "Golf", color: "Black"}, airplane{number: "Bis", horses: "Blue"}
+		list []vehicleInterface
 	)
-	var list []vehicleInterface
 	list = append(list, c, m)
+	stop(list)
 
 	for _, y := range list {
 		y.turnOn()
@@ -37,6 +38,27 @@ func (c car) turnOn() {
 	fmt.Println("Car on!")
 }
 
-func (m moto) turnOn() {
-	fmt.Println("Moto on!")
+func (m airplane) turnOn() {
+	fmt.Println("Airplane on!")
+}
+
+func stop(v []vehicleInterface) {
+	// Use interface to manipulate what type is on the fly
+	type carCheck interface {
+		vrumvrum()
+	}
+	// List received
+	for _, it := range v {
+		c, ok := it.(carCheck)
+		if !ok {
+			continue
+		}
+
+		c.vrumvrum()
+	}
+
+}
+
+func (c car) vrumvrum() {
+	fmt.Printf("vrum \n")
 }
