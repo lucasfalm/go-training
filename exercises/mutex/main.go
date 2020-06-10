a34f818b12f93a5f646931d12e86328c276a9c75
flucas97 <feijolucas1997@gmail.com>
2020-06-10 19:38:54 -0300

    use mutex to lock race concurrency

---
+++ exercises/mutex/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	counter := 0
+
+	const gs = 100
+	var mu sync.Mutex
+
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			mu.Lock() // lock the race
+			y := &counter
+			runtime.Gosched() // goroutine sleep
+			*y++              // increment
+			mu.Unlock()       // unlock thread
+			wg.Done()         // finish goroutine
+		}()
+		fmt.Printf("Thread %v and counter is: %v\n", i, counter)
+	}
+	wg.Wait() // wait for all goroutines end
+
+	fmt.Println("The final value of counter is:", counter)
+
+}
