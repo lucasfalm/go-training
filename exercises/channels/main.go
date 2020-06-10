7dee4d35d6f91095113423ab138957932b26d39d
flucas97 <feijolucas1997@gmail.com>
2020-06-10 20:05:32 -0300

    some comments about bidirecional channels

--- exercises/channels/main.go
+++ exercises/channels/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	// you can configure if the channels is just for read, write...
+	// make(<-chan int) -> receive channel
+	// make(chan<- int) -> send channel
 	c := make(chan int, 1) // create channel with 1 goroutine
 	c <- 42
 	//go func() { // init goroutine
