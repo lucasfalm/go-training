9b9fd51c63d86e83c66565e31f71f8e03a351046
flucas97 <feijolucas1997@gmail.com>
2020-06-10 20:00:57 -0300

    another way to create a channel

--- exercises/channels/main.go
+++ exercises/channels/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	c := make(chan int) // create channel
-
-	go func() { // init goroutine
-		c <- 42 // send 42 to channel
-	}()
+	c := make(chan int, 1) // create channel with 1 goroutine
+	c <- 42
+	//go func() { // init goroutine
+	//	c <- 42 // send 42 to channel
+	//}()
 
 	fmt.Println(<-c) // take the value of channel
 }
