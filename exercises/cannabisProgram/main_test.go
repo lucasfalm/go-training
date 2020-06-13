03763ed890ed53ff63ef49505d43db5a3c9505fd
flucas97 <feijolucas1997@gmail.com>
2020-06-13 13:10:31 -0300

    create test for updateFlower

--- exercises/cannabisProgram/main_test.go
+++ exercises/cannabisProgram/main_test.go
@@ -1,44 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func setup(t *testing.T) *cannabis {
-	cExample := cannabis{
-		name: "Test",
-	}
-	return &cExample
+type cannabisFixture struct {
+	testName     string
+	expectError  bool
+	errorMessage string
+
+	mockCannabis *cannabis
 }
+
+func (c *cannabisFixture) setup(t *testing.T) {
+	c.mockCannabis = newCannabisMock()
+}
+
 func TestUpdateFlower(t *testing.T) {
-	type test struct {
-		f    *cannabis
-		fail bool
-	}
-	f := setup(t)
-
-	tests := []test{
-		test{f, false},
-	}
-
-	for _, t := range tests {
-		_ = t
-	}
-	nF := flowers{
-		name: "Test",
-		thc:  35,
-		geo:  make(map[string]int), // Initializing map
-	}
-	eX := map[string]int{
-		"test": 10,
-	}
-	err := f.updateFlower("Test", 35, eX)
-
-	if err != nil {
-		t.Error(f)
+	table := []cannabisFixture{
+		{
+			testName:    "Create new flower",
+			expectError: false,
+		},
+		{
+			testName:    "Create new flower with error",
+			expectError: true,
+		},
 	}
 
-	if f.flowers[0].name != nF.name {
-		t.Error(f)
+	for index, f := range table {
+		t.Run(fmt.Sprintf("%v-%v", index, f.testName), func(t *testing.T) {
+			f.setup(t)
+
+			eX := map[string]int{
+				"Brazil": 1500,
+			}
+
+			err := f.mockCannabis.updateFlower("Test", 35, eX)
+			if !f.expectError {
+				if err != nil {
+					t.Fatal("an error was not expected, but occurred")
+				}
+			}
+
+			err = f.mockCannabis.updateFlower("Test", -35, eX)
+			if f.expectError {
+				if err == nil {
+					t.Fatal("an error was expected, but not occurred")
+				}
+			}
+		},
+		)
+	}
+}
+
+func newCannabisMock() *cannabis {
+	return &cannabis{
+		name: "Sativa",
 	}
 }
