0f1072579f29266c7796c3cd9a5ba4fe19948a6f
flucas97 <feijolucas1997@gmail.com>
2020-06-14 18:38:33 -0300

    handler routes

--- feedApp/httpd/handler/feedapp_post.go
+++ feedApp/httpd/handler/feedapp_post.go
@@ -7,9 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type feedAppPostRequest struct {
+	Title string `json:"title"`
+	Post  string `json:"post"`
+}
+
 // FeedAppPost handle POST on feedapp
 func FeedAppPost(feed *feedapp.Repo) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		requestBody := feedAppPostRequest{}
+
+		c.Bind(&requestBody)
+
 		c.JSON(http.StatusOK, map[string]string{
 			"hello": "Found",
 		})
