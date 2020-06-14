7f9ad6eefe5c8c9db2a9a8de8ee8281ce3824878
flucas97 <feijolucas1997@gmail.com>
2020-06-14 18:59:47 -0300

    add mux and mongo

--- mongo-go/go.mod
+++ mongo-go/go.mod
@@ -1,3 +1,8 @@
 module github.com/flucas97/go-trainning/mongo-go
 
 go 1.14
+
+require (
+	github.com/gorilla/mux v1.7.4 // indirect
+	go.mongodb.org/mongo-driver v1.3.4 // indirect
+)
---
+++ mongo-go/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
+var client *mongo.Client
+
+func main() {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	router := mux.NewRouter()
+	http.ListenAndServe(":12345", router)
+}
