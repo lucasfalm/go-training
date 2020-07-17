8826cbdd505ad7e3ae0893d1867ed3ed4080b07e
flucas97 <feijolucas1997@gmail.com>
2020-07-17 17:37:24 -0300

    refactor, but got errors

--- exercises/Codility/Counting/FindMinimalPositive/main.go
+++ exercises/Codility/Counting/FindMinimalPositive/main.go
@@ -6,38 +6,76 @@ import (
 )
 
 func main() {
-	elements := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	elements := []int{-100, 1}
 	fmt.Println("Solution: ", Solution(elements))
 }
 
 func Solution(A []int) int {
 	sort.Ints(A)
-	start, stop, missing, perfectCount := A[0], A[len(A)-1:], 0, make(map[int]bool)
+	start, stop, missing, perfectCount, lowerValue := A[0], A[len(A)-1:], 0, make(map[int]bool), 0
 
-	if start > 0 && stop[0] > 0 {
-		for i := start; i <= stop[0]; i++ {
-			perfectCount[i] = false
+	if len(A) == 1 {
+		if A[0] > 2 {
+			return A[0] - 1
+		} else if A[0] == 1 {
+			return 2
+		} else {
+			return 1
 		}
-
-		for i := 0; i <= len(A)-1; i++ {
-			actualValue := A[i]
-
-			if _, found := perfectCount[actualValue]; found {
-				perfectCount[actualValue] = true
-			}
-		}
-
-		for missingValue, status := range perfectCount {
-			if !status {
-				missing = missingValue
-			}
-		}
-		if missing == 0 {
-			missing = stop[0] + 1
-		}
-
-		return missing
 	} else {
-		return 1
+		if start < 0 && stop[0] < 0 {
+			missing = 1
+		} else if start < 0 && stop[0] == 1 {
+			missing = stop[0] + 1
+		} else {
+			for i := start; i <= stop[0]; i++ {
+				perfectCount[i] = false
+			}
+
+			for i := 0; i <= len(A)-1; i++ {
+				actualValue := A[i]
+
+				if _, found := perfectCount[actualValue]; found {
+					perfectCount[actualValue] = true
+				}
+			}
+
+			before := 0
+
+			for missingValue, status := range perfectCount {
+				if !status {
+					if missing == 0 {
+						missing = missingValue
+						lowerValue = missing
+					} else {
+						before = missing
+						missing = missingValue
+
+						if before < missing {
+							lowerValue = before
+						} else {
+							lowerValue = missing
+						}
+					}
+				}
+			}
+
+			if start > 1 && lowerValue > 1 {
+				lowerValue = 1
+			}
+			missing = lowerValue
+		}
 	}
+
+	if missing == 0 {
+		if start > 1 && stop[0] > 1 {
+			missing = 1
+		} else if start < 0 && stop[0] > 1 {
+			missing = stop[0] + 1
+		} else {
+			missing = start + 1
+		}
+	}
+
+	return missing
 }
