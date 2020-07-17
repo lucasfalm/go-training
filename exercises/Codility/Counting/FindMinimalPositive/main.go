136f13a2411784d1cc92b6969229aebea9ca8fa5
flucas97 <feijolucas1997@gmail.com>
2020-07-17 20:55:12 -0300

    refactor, and 55%

--- exercises/Codility/Counting/FindMinimalPositive/main.go
+++ exercises/Codility/Counting/FindMinimalPositive/main.go
@@ -6,75 +6,76 @@ import (
 )
 
 func main() {
-	elements := []int{-100, 1}
+	elements := []int{1, 1, 2, 3, 4, 6, 7, 9}
 	fmt.Println("Solution: ", Solution(elements))
 }
 
 func Solution(A []int) int {
-	sort.Ints(A)
-	start, stop, missing, perfectCount, lowerValue := A[0], A[len(A)-1:], 0, make(map[int]bool), 0
+	totalSliceLenght, missing := len(A), 0
 
-	if len(A) == 1 {
-		if A[0] > 2 {
-			return A[0] - 1
-		} else if A[0] == 1 {
-			return 2
-		} else {
-			return 1
-		}
-	} else {
-		if start < 0 && stop[0] < 0 {
-			missing = 1
-		} else if start < 0 && stop[0] == 1 {
-			missing = stop[0] + 1
-		} else {
-			for i := start; i <= stop[0]; i++ {
-				perfectCount[i] = false
+	if totalSliceLenght >= 1 {
+		sort.Ints(A)
+		start, stop, perfectCount := A[0], A[len(A)-1:], make(map[int]bool)
+
+		if totalSliceLenght == 1 {
+			if start > 2 {
+				return start - 1
+			} else if start == 1 {
+				return 2
+			} else {
+				return 1
 			}
-
-			for i := 0; i <= len(A)-1; i++ {
-				actualValue := A[i]
-
-				if _, found := perfectCount[actualValue]; found {
-					perfectCount[actualValue] = true
+		} else if totalSliceLenght == 2 {
+			if start > 1 && stop[0] > 1 {
+				return 1
+			} else {
+				return start + 1
+			}
+		} else {
+			if start < 0 && stop[0] < 0 {
+				missing = 1
+			} else {
+				if start > 1 && stop[0] > 1 {
+					return 1
 				}
-			}
 
-			before := 0
+				for increment := start; increment <= stop[0]; increment++ {
+					perfectCount[increment] = false
+					if increment == 0 {
+						perfectCount[increment] = true
+					}
+				}
 
-			for missingValue, status := range perfectCount {
-				if !status {
-					if missing == 0 {
-						missing = missingValue
-						lowerValue = missing
-					} else {
-						before = missing
-						missing = missingValue
+				for i := 0; i <= len(A)-1; i++ {
+					actualValue := A[i]
 
-						if before < missing {
-							lowerValue = before
-						} else {
-							lowerValue = missing
-						}
+					if _, found := perfectCount[actualValue]; found {
+						perfectCount[actualValue] = true
+					}
+				}
+
+				for value, _ := range perfectCount {
+					if !perfectCount[value] {
+						missing = value
+						break
 					}
 				}
 			}
 
-			if start > 1 && lowerValue > 1 {
-				lowerValue = 1
+			if missing == 0 {
+				if start > 1 && stop[0] > 1 {
+					missing = 1
+				} else if start < 0 && stop[0] > 1 {
+					missing = stop[0] + 1
+				} else {
+					missing = stop[0] + 1
+				}
+			} else if missing < 0 {
+				missing = 1
 			}
-			missing = lowerValue
 		}
-	}
-
-	if missing == 0 {
-		if start > 1 && stop[0] > 1 {
-			missing = 1
-		} else if start < 0 && stop[0] > 1 {
-			missing = stop[0] + 1
-		} else {
-			missing = start + 1
-		}
+	} else {
+		return 1
 	}
 
 	return missing
