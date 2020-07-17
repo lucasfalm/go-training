760953658fc65a56c86225f162cd47405747d35d
flucas97 <feijolucas1997@gmail.com>
2020-07-16 23:45:45 -0300

    refactor

--- exercises/Codility/Arrays/main.go
+++ exercises/Codility/Arrays/main.go
@@ -3,29 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	key := 3
-
-	fmt.Println(Solution(numbers, key))
+	numbers := []int{1, 2, 3, 4} // [len(numbers)-y:len(numbers - i)]
+	key := 13
+	fmt.Println("Original", numbers)
+	fmt.Println("Solution: ", Solution(numbers, key))
 }
 
 func Solution(A []int, K int) []int {
 	result := []int{}
+	x, y := 1, 0
 
 	for i := 1; i <= K; i++ {
-		if len(A) >= 1 {
-			nextLength := len(A) - 1
-			last := A[nextLength:]
-			A = A[:nextLength]
-			result = append([]int{last[0]}, result...)
+		last := A[len(A)-x : len(A)-y]
+		fmt.Println(last)
+		x++
+		y++
+
+		result = append([]int{last[0]}, result...)
+
+		if y >= len(A) {
+			x, y = 1, 0
 		}
 	}
 
-	if len(A) > 0 {
-		for _, number := range A {
-			result = append(result, number)
+	if len(A) >= K {
+		less := A[:len(A)-len(result)]
+		for _, v := range less {
+			result = append(result, v)
 		}
 	}
 
-	return result
+	return result[:len(A)]
 }
