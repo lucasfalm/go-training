1e6d92f5a437d1521e371a2c685eb92a705c93d5
Lucas Feijo <feijolucas1997@gmail.com>
2020-10-14 10:24:02 -0300

    feat: equalize challange, Big O(n) space and time

---
+++ exercises/equalize-array/main.go
@@ -0,0 +1,22 @@
+
+//https://www.hackerrank.com/challenges/equality-in-a-array/problem
+// Big O(n) time, and O(2n) space
+func equalizeArray(arr []int32) int32 {
+    var ( 
+        rankRepeated = map[int32]int{}
+        maxRepeated int
+    )
+
+    for _, actualNumber := range arr { 
+        rankRepeated[actualNumber]++
+    }
+
+    for _, repeated := range rankRepeated { 
+        if repeated > maxRepeated {
+            maxRepeated = repeated
+        }
+    }
+
+    return int32((len(arr)) - maxRepeated)
+}
+
