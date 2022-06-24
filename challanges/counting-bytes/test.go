d376177f55b2a57aece22ebb754b3531f96efc18
Lucas Feijo <feijolucas1997@gmail.com>
2022-06-24 16:57:15 -0300

    :sparkles: feat: counting bytes

---
+++ challanges/counting-bytes/test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'countBits' function below.
+ *
+ * The function is expected to return an int32.
+ * The function accepts unit32 num as parameter.
+ */
+
+func countBits(num uint32) int32 {
+	count := uint32(0)
+	for num != 0 {
+		count += num & 1
+		num >>= 1
+	}
+	return int32(count)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	numTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	num := uint32(numTemp)
+
+	result := countBits(num)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
