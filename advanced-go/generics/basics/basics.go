c65dd6438f5c992d1c326aa208614f7b17bf97b9
Lucas Feijo <feijolucas1997@gmail.com>
2024-03-27 22:47:00 +1300

    :sparkles: Feat: explaning about the usage of "~" with generics

--- advanced-go/generics/basics/basics.go
+++ advanced-go/generics/basics/basics.go
@@ -2,19 +2,29 @@ package basics
 
 import "fmt"
 
-type ShowItParameter interface {
-	int32 | float64
+// NOTE: the "~" makes it accept the type and all the other types that underlay the type
+//
+//	a.k.a myIntAlias, which is another type not specified here, but with the underling
+//	type being one of the listed (int);
+type showItParams interface {
+	~int32 | ~float64 | ~int
 }
 
-func ShowIt[T ShowItParameter](numberOne, numberTwo T) {
+type myIntAlias int
+
+func ShowIt[T showItParams](numberOne, numberTwo T) {
 	fmt.Printf("adding %v + %v using generics = %v\n", numberOne, numberTwo, addX(numberOne, numberTwo))
+
+	var intAlias myIntAlias = 1
+
+	fmt.Printf("this just works because of the til (~) = %v + %v = %v\n", intAlias, intAlias, addX(intAlias, intAlias))
 }
 
 // NOTE: it accepts both int32 or float64
 //
 //	two ways of defining the type:
 //	1. like this
-//	2. or using the ShowItParameter which has the type constraint
-func addX[T int32 | float64](a, b T) T {
+//	2. or using the showItParams which has the type constraint
+func addX[T ~int32 | ~float64 | ~int](a, b T) T {
 	return a + b
 }
